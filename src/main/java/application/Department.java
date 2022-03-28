03ccf3000a87caca8b1cb5869c733c77f4a63a59
KilianDuerrschmid <k.duerrschmid@gmx.at>
2022-03-28 15:30:59 +0200

    finished without extra tasks

--- src/main/java/application/Department.java
+++ src/main/java/application/Department.java
@@ -1,12 +1,35 @@
 package application;
 
 public class Department {
+
+	String name;
+	int id;
+	String country;
+	String city;
+
 	/*
 	 *
 	 * Your code comes here. Remember you can overload constructors.
 	 *
 	 */
+
+	public Department(String name, int id, String country, String city){
+		this.name = name;
+		this.id = id;
+		this.country = country;
+		this.city = city;
+
+	}
+
+	//default Department
+	public Department(){
+		name = "Test";
+		id = 123;
+		country = "Neverland";
+		city = "Gotham";
+	}
+
 	public boolean isEquals(Department other) {
-		return false;
+		return id == other.id;
 	}
 }
--- src/main/java/application/Employee.java
+++ src/main/java/application/Employee.java
@@ -3,16 +3,37 @@ package application;
 public class Employee {
 	final Department dept;
 
+	String firstName;
+	String lastName;
+	int id;
+
+
+	public Employee(Department dept, String firstName, String lastName, int id){
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.id = id;
+		this.dept = dept;
+	}
+
+	public Employee(){
+		firstName = "Max";
+		lastName = "Mustermann";
+		id = 123;
+		dept = new Department();
+	}
+
+
+
 	/*
 	 *
 	 * Your code comes here. Remember you can overload constructors.
 	 *
 	 */
 	public void print() {
-		System.out.println();
+		System.out.println("Der Mitarbeiter " + id + " " + firstName + " " + lastName + " arbeitet in der Abteilung " + dept.name + " in " + dept.city);
 	}
 
 	public boolean isEquals(Employee other) {
-		return false;
+		return id == other.id;
 	}
 }
